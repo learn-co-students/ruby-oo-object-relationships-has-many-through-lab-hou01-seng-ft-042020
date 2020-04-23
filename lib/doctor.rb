f265dc8d7b45db47dbdf689904ad288c255dd71c
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-23 17:47:12 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+attr_accessor :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name=name 
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(patient, date)
+       appointment= Appointment.new(date, patient, self)
+    end 
+
+    def appointments
+        Appointment.all.select do |x| x.doctor == self 
+        end
+    end
+
+    def patients
+        appointments.map do |x| x.patient end
+    end
+
+end
